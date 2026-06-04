6ff470f57c14e718ba845d3d50a090340b02e12a
javi-sort <javier.romero555@gmail.com>
2026-06-04 23:20:23 +1000

    Found my issue! Request not meeting required fields oops

--- src/models/Listing.ts
+++ src/models/Listing.ts
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 const ListingSchema = new mongoose.Schema(
     {
         url: { type: String, required: true },
-        suburb: { type: String, required: true },
-        price: { type: Number, required: true },
-        beds: { type: Number, required: true },
-        baths: { type: Number, required: true },
+        suburb: { type: String },
+        price: { type: String, required: true },
+        beds: { type: Number },
+        baths: { type: Number },
         isActive: { type: Boolean, default: true },
         agentName: { type: String },
         agentPhone: { type: String },
